d17323abd173cf288c058273392ca412a53dc30c
Andreas Tsatsanis <code@andreats.com>
2025-07-11 11:20:39 +0200

    cargo fmt

--- crates/lib/src/can/mod.rs
+++ crates/lib/src/can/mod.rs
@@ -1,7 +1,6 @@
 //! two separate instances give type restrictions for which bus to use
-pub mod fdcan;
 pub mod can2;
-// todo: 
+pub mod fdcan;
+// todo:
 // + trait to unify FdEnvelope and Envelope
 // + trait to unify CAN and FDCAN
-
